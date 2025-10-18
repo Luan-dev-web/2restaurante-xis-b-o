133ff88cc7b5146f9cee258c909e748d9e90d169
Luan-dev-web <luan.fassinape@gmail.com>
2025-10-18 09:48:59 -0300

    xis-capa

--- script.js
+++ script.js
@@ -2,10 +2,17 @@ document.addEventListener("DOMContentLoaded", () => {
     const menuIcon = document.getElementById('menuIcon');
     const menuOverlay = document.getElementById('menuOverlay');
     const paragrafoXis = document.getElementById('paragrafoXis');/*Para o Js saber oque é para usar depois(aqui em baixo)*/ 
-  
+    const RestoDoSite = document.getElementById('RestoDoSite');
+
     menuIcon.addEventListener('click', () => {
       menuOverlay.classList.toggle('active');
       menuIcon.classList.toggle('active');
       paragrafoXis.classList.toggle('active');
+
+      if(menuOverlay.classList.contains('active')){
+        RestoDoSite.style.display = 'none'; // esconde o conteúdo
+      } else {
+        RestoDoSite.style.display = 'block'; // mostra de volta
+      }  
     });
   });
