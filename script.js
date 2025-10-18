fd69909bb79cf0f25abda2603a9889bb0e2a5e3f
Luan-dev-web <luan.fassinape@gmail.com>
2025-10-18 07:49:58 -0300

    menu-completo

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     const menuIcon = document.getElementById('menuIcon');
     const menuOverlay = document.getElementById('menuOverlay');
+    const paragrafoXis = document.getElementById('paragrafoXis');/*Para o Js saber oque é para usar depois(aqui em baixo)*/ 
   
     menuIcon.addEventListener('click', () => {
       menuOverlay.classList.toggle('active');
+      menuIcon.classList.toggle('active');
+      paragrafoXis.classList.toggle('active');
     });
   });
