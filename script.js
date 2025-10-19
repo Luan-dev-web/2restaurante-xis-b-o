6a20e42ed0d767869e027314fe75b06051db6f9a
Luan-dev-web <luan.fassinape@gmail.com>
2025-10-19 20:47:25 -0300

    emcima-pronto

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const menuOverlay = document.getElementById('menuOverlay');
     const paragrafoXis = document.getElementById('paragrafoXis');/*Para o Js saber oque é para usar depois(aqui em baixo)*/ 
     const RestoDoSite = document.getElementById('RestoDoSite');
+    const menu = document.getElementById('menu');
 
     menuIcon.addEventListener('click', () => {
       menuOverlay.classList.toggle('active');
       menuIcon.classList.toggle('active');
       paragrafoXis.classList.toggle('active');
+      menu.classList.toggle('menu');
 
       if(menuOverlay.classList.contains('active')){
         RestoDoSite.style.display = 'none'; // esconde o conteúdo
